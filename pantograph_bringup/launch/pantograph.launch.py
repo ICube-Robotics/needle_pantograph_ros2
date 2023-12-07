0dfd6bbfcf73092656a3109e73b42ea00d0e0801
Thibault Poignonec <thibault.poignonec@gmail.com>
2023-12-07 16:41:19 +0100

    add basic bringup pkg for test

---
+++ pantograph_bringup/launch/pantograph.launch.py
@@ -0,0 +1,116 @@
+# Copyright 2023 ICube Laboratory, University of Strasbourg
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from launch.actions import DeclareLaunchArgument
+from launch import LaunchDescription
+from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution
+
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+
+def generate_launch_description():
+    # Declare arguments
+    declared_arguments = []
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            'use_sim',
+            default_value='false',
+            description='Start robot in Gazebo simulation.',
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            'use_fake_hardware',
+            default_value='false',
+            description='Start robot with fake hardware mirroring command to its states.',
+        )
+    )
+
+    use_sim = LaunchConfiguration('use_sim')
+    use_fake_hardware = LaunchConfiguration('use_fake_hardware')
+
+    # Get URDF via xacro
+    robot_description_content = Command(
+        [
+            PathJoinSubstitution([FindExecutable(name='xacro')]),
+            ' ',
+            PathJoinSubstitution(
+                [FindPackageShare('pantograph_description'), 'config', 'pantograph.config.xacro']
+            ),
+            ' ',
+            'use_sim:=', use_sim,
+            ' ',
+            'use_fake_hardware:=', use_fake_hardware,
+        ]
+    )
+    robot_description = {'robot_description': robot_description_content}
+
+    ros2_controllers = PathJoinSubstitution(
+        [
+            FindPackageShare('pantograph_description'),
+            'config',
+            'ros2_controllers.yaml',
+        ]
+    )
+
+    control_node = Node(
+        package='controller_manager',
+        executable='ros2_control_node',
+        parameters=[robot_description, ros2_controllers],
+        output='both',
+    )
+    robot_state_pub_node = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='both',
+        parameters=[robot_description],
+    )
+    rviz_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        output='log',
+        # arguments=['-d', rviz_config_file],
+    )
+
+    joint_state_broadcaster_spawner = Node(
+        package='controller_manager',
+        executable='spawner',
+        arguments=['joint_state_broadcaster'],
+    )
+
+    trajectory_controller_spawner = Node(
+        package='controller_manager',
+        executable='spawner',
+        arguments=['haptic_trajectory_controller'],
+        )
+
+    '''
+    effort_controller_spawner = Node(
+        package='controller_manager',
+        executable='spawner',
+        arguments=['forward_effort_controller'],
+        )
+    '''
+
+
+    nodes = [
+        control_node,
+        rviz_node,
+        robot_state_pub_node,
+        joint_state_broadcaster_spawner,
+    ]
+
+    return LaunchDescription(declared_arguments + nodes)
